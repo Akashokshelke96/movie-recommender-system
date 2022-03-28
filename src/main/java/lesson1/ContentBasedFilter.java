79043e7a341d5d86610741fcb15184c8f0b4e437
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-03-28 14:05:16 +0530

    initial commit

---
+++ src/main/java/lesson1/ContentBasedFilter.java
@@ -0,0 +1,8 @@
+package lesson1;
+
+public class ContentBasedFilter {
+    //logic of content based filter
+    public String[] getRecommendations(String movies) {
+        return new String[]{"Happy feet", "Ice Age", "Shark Tale"};
+    }
+}
